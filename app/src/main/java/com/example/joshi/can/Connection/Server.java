455a0ab2f6119876e22566315c45309df8cfab2b
Cedde97 <ceddo.alt@gmail.com>
2017-09-02 13:13:06 +0200

    Update Server.java

--- app/src/main/java/com/example/joshi/can/Connection/Server.java
+++ app/src/main/java/com/example/joshi/can/Connection/Server.java
@@ -1,161 +1,172 @@
-package com.example.joshi.can.Connection;
-
-import android.util.Log;
-
-import com.example.joshi.can.Logic.Node;
+package helper;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server extends Thread {
-    String methodName;
+@SuppressWarnings("unused")
+public class Server {
+	ServerSocket serverSocket = null;
+	Serialization serialization = new Serialization();
+	
+	
+	
+	/**
+	 * Methode zum Empfangen eines ByteArrays des Senders
+	 * 
+	 * @param serverSocket 		= ServerSocket, auf dem Empfangen wird
+	 * @return					= ByteArray, das gesendet wurde
+	 * @throws IOException		= Fehler bei Input/Output
+	 */
+	
+	protected byte[] receiveByteArray(ServerSocket serverSocket, Socket socket) throws IOException{
+		
+		byte[] buffer = null;
+		
+		try{
+			
+			DataInputStream dIn = new DataInputStream(socket.getInputStream());
 
-    public static void main(String[] args) throws Exception {
-        Server server = new Server();
-        server.run();
+			int length = dIn.readInt();  
+			
+				if(length>0) {
+				    buffer = new byte[length];
+				    dIn.readFully(buffer, 0, buffer.length);
+				}
+				
+			try{
+				if(serverSocket!= null){
+					serverSocket.close();
+				}
+			} catch(IOException e){
+				e.printStackTrace();
+			}
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+		return buffer;
+	}
+	
+	
+	
+	/**
+	 * Methode, um ein ByteArray in ein Bild zu konvertieren
+	 * 
+	 * @param buffer 			= das ByteArray, das konvertiert werden soll
+	 * @return 					= die File/das Image
+	 */
+	
+	protected void saveFileFromByteArray(byte[] buffer, File file){
+		
+		try{
+			serialization.imageDeSerializer(buffer, file);
+			
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		
 
-    }
-
-    @Override
-    public void run() {
-        try {
-            Node node = new Node();
-            System.out.println("Server is started");
-            ServerSocket ss = new ServerSocket(8080);
-
-            System.out.println("Server is waiting for request");
-            Socket s = ss.accept();
-
-            System.out.println("Cliet Connected");
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            String str = br.readLine();
-
-            System.out.println("Client Data: " + str);
-
-            ss.close();
-            methodName = returnMethodName(str);
-            switch(methodName){
-                case "requestJoin": System.out.println("in RequestJoin\n");
-                                    break;
-
-                case "routing":     System.out.println("in routing\n");
-                                    break;
-
-                case "receiveRoutingRequest": System.out.println("in receiveRoutingRequest\n");
-                                    break;
-                case "hashX": System.out.println("hashX-Value:" +node.hashX(returnNewIP(str)));
-
-            }
-
-            //println Block zum Testen/Sehen der Ergebnisse
-            System.out.println("");
-            System.out.println("MethodName :" + returnMethodName(str));
-            System.out.println("X-Koordinate: " + returnXCoordinate(str));
-            System.out.println("Y-Koordinate: " + returnYCoordinate(str));
-            System.out.println(returnInteger(str));
-            System.out.println("");
-            System.out.println("Beweis doubles werden mit 0,25 addiert");
-            System.out.println("X_Neu : " + beweis(returnXCoordinate(str)));
-            System.out.println("Y_Neu : " + beweis(returnYCoordinate(str)));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Hilfsmethoden, die den uebergebenen String zurecht schneiden
-     * <p>
-     * der String wurde vorerst durch ',' , 'X' , 'Y' , 'I'
-     * <p>
-     * an den entscheidenden Stellen unterteilt
-     *
-     * @param longterm = der gesamte gesendete String
-     * @return der jeweiligen Parameter
-     */
-
-    private static String returnMethodName(String longterm) {
-
-        int index = longterm.indexOf(',');
-
-        String methodName = longterm.substring(0, index);
-        return methodName;
-    }
-
-    public static String giveMethodName(String str){
-        return returnMethodName(str);
-    }
-
-    private static double returnXCoordinate(String longterm) {
-
-        int indexOfKomma = longterm.indexOf('N');
-        indexOfKomma += 1;
-        int indexOfX = longterm.indexOf('x');
-
-
-        String x = longterm.substring(indexOfKomma, indexOfX);
-        double x1 = Double.parseDouble(x);
-        return x1;
-    }
-
-    public static double giveXCoord(String str){
-        return returnXCoordinate(str);
-
-    }
-
-    private static String returnNewIP(String longterm){
-        int indexOfKomma = longterm.indexOf(',');
-        indexOfKomma += 1;
-        int indexOfNIP = longterm.indexOf('N');
-
-        String nip = longterm.substring(indexOfKomma, indexOfNIP);
-        return nip;
-
-    }
-
-    private static double returnYCoordinate(String longterm) {
-
-        int indexOfKomma = longterm.indexOf('x');
-        indexOfKomma += 1;
-        int indexOfY = longterm.indexOf('Y');
-
-
-        String y = longterm.substring(indexOfKomma, indexOfY);
-        double y1 = Double.parseDouble(y);
-        return y1;
-
-    }
-
-    public static double giveYCoord(String str){
-        return returnYCoordinate(str);
-
-    }
-
-    private static int returnInteger(String longterm) {
-
-        int indexOfKomma = longterm.indexOf('Y');
-        indexOfKomma += 1;
-        int indexOfInteger = longterm.indexOf('I');
-
-
-        String i = longterm.substring(indexOfKomma, indexOfInteger);
-        int i1 = Integer.parseInt(i);
-
-        return i1;//("Integer-Wert: " + i1);
-    }
-
-    public static int giveInt(String str){
-        return returnInteger(str);
-    }
-
-    private static double beweis(double wert) {
-        wert += 0.25;
-        return wert;
-    }
-
-    public String getMethodName(){
-        return methodName;
-    }
+	}
+	
+	
+	
+	/**
+	 * Methode um eine File zu speichern
+	 * @param file		= die File, die gespeichert werden soll
+	 * @param path		= das Path zum Directory, in dem sie erstellt werden soll
+	 */
+	
+	protected void saveFile(File file, String path){
+		String destination = (path + file.getName());
+		Writer output = null;
+		File newFile = new File(destination);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * HilfsMethoden zum Cutten (wird noch ersetzt)
+	 * 
+	 */
+	private static String returnMethodName(String longterm){
+		
+		int index = longterm.indexOf(',');
+		
+		String methodName = longterm.substring(0,index);
+		return ("MethodName: " + methodName);
+	}
+	
+	private static String returnSecondString(String longterm){
+		
+		int indexOfKomma	 = longterm.indexOf(',');
+			indexOfKomma 	+= 1;
+		int indexOfAt 	 = longterm.indexOf('@');
+	    
+	    
+		
+	
+	    String secondString = longterm.substring(indexOfKomma, indexOfAt);
+		return ("SecondString: " + secondString + "index of N" + indexOfAt );
+	}
+	
+	
+	private static String returnXCoordinate(String longterm){
+		
+		int indexOfAt 	 = longterm.indexOf('@');
+		    indexOfAt	+= 1;
+		int indexOfX 		 = longterm.indexOf('X');
+			
+		
+		String x = longterm.substring(indexOfAt, indexOfX);
+		double x1 = Double.parseDouble(x);
+		return (x1 + "index of X" + indexOfX);
+	}
+	
+	private static double returnYCoordinate(String longterm){
+	
+		int indexOfKomma 	 = longterm.indexOf('X');
+		    indexOfKomma 	+= 1;
+		int indexOfY 		 = longterm.indexOf('Y');
+			
+		
+		String y = longterm.substring(indexOfKomma, indexOfY);
+		double y1 = Double.parseDouble(y);
+		return y1;
+		
+	}
+	
+	private static int returnInteger(String longterm){
+		
+		int indexOfKomma 	 = longterm.indexOf('Y');
+		    indexOfKomma 	+= 1;
+		int indexOfInteger   = longterm.indexOf('I');
+			
+		
+		String i = longterm.substring(indexOfKomma, indexOfInteger);
+		int i1   = Integer.parseInt(i);
+		
+		return i1;
+	}
+	
+	private static double beweis (double wert){
+		wert += 0.25;
+		return wert;
+	}
 }
